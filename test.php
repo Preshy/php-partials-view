2ef796841fe9946b041f40a6debc9b52ff39e835
Precious Opusunju <haxorpreshy@gmail.com>
2017-07-07 09:39:01 +0100

    removed much spacing

--- test.php
+++ test.php
@@ -1,32 +1,25 @@
 <?php
 
 /*
-
 Load class
-
 */
 
 include "src/Views.php";
 
 /* Set root */
-
 $root =  $_SERVER['DOCUMENT_ROOT'];
-
 /* Set partials */
 
 $header = "header.php";
 $footer = "footer.php";
 
 /*
-
 Initialize class
-
 */
 
 $views = new \PPV\Views($root, $header, $footer);
 
 /* Load body via this little url route */
-
 $uri = explode('/', $_SERVER['REQUEST_URI']);
 
 switch($uri[1]){
